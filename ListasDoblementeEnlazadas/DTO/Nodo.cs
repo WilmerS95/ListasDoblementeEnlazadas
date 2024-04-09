0a465f2e802ff53dc89bf6051969bec674b7baa9
WilmerS95 <wilmerantoniosinay@gmail.com>
2024-04-09 08:11:41 -0600

    Creación Nodo DTO

---
+++ ListasDoblementeEnlazadas/DTO/Nodo.cs
@@ -0,0 +1,15 @@
+﻿namespace ListasDoblementeEnlazadas.DTO
+{
+    public class Nodo
+    {
+        public Nodo? ligaAnterior { get; set; }
+        public string informacion { get; set; }
+        public Nodo? ligaSiguiente { get; set; }
+        public Nodo(string Informacion)
+        {
+            ligaAnterior = null; 
+            ligaSiguiente = null; 
+            informacion = Informacion;
+        }
+    }
+}
