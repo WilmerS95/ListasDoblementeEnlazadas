36e3dbf109ae896677f61c9af338a047300a3e5f
rocio <rocio@ROCIOLAP>
2024-04-10 22:23:39 -0600

    Se implemento  funciones de agregar video dépues posición especifica y función de eliminar al final

--- ListasDoblementeEnlazadas/Services/ListasDoblementeEnlazadas.cs
+++ ListasDoblementeEnlazadas/Services/ListasDoblementeEnlazadas.cs
@@ -35,6 +35,8 @@ namespace ListasDoblementeEnlazadas.Services
             return "Se ha agregado al inicio de la lista";
         }
 
+    
+
         public string AddBeforeVideoX(string videoX, Nodo nuevoNodo)
         {
             if (isEmpty)
@@ -68,6 +70,71 @@ namespace ListasDoblementeEnlazadas.Services
             }
             return "El link del video específico no se encontró en la lista.";
         }
+
+        public string AddAfterSpecificPosition(int posicion, Nodo nuevoNodo)
+        {
+            if (isEmpty)
+            {
+                return "La lista esta vacia.";
+            }
+            nodoActual = primerNodo;
+            int contador = 1;
+
+            while (nodoActual != null && contador < posicion)
+            {
+                nodoActual = nodoActual.ligaSiguiente;
+                contador++;
+            }
+
+            if (nodoActual != null)
+            {
+                nuevoNodo.ligaSiguiente = nodoActual.ligaSiguiente;
+                if(nodoActual.ligaSiguiente != null) 
+                {
+                    nodoActual.ligaSiguiente.ligaAnterior = nuevoNodo;
+                }
+                nuevoNodo.ligaAnterior = nodoActual;
+                nodoActual.ligaSiguiente = nuevoNodo;
+                
+                if (nuevoNodo.ligaSiguiente == null)
+                {
+                    ultimoNodo = nuevoNodo;
+                }
+
+                return "Se ha agregado el video después de la posición específica.";
+            }
+            else
+            {
+                return "La posición específica no se encontró en la lista.";
+            }
+        }
+
+       
+        public string EliminarNodoAlFinal()
+        {
+
+            if (isEmpty)
+            {
+                return "No existen elementos";
+            }
+            else if (primerNodo == ultimoNodo)
+            {
+                primerNodo = ultimoNodo = null;
+            }
+            else
+            {
+                Nodo nodoEliminar;
+
+                nodoEliminar = ultimoNodo;
+                ultimoNodo = ultimoNodo.ligaAnterior;
+                ultimoNodo.ligaSiguiente = null;
+
+                nodoEliminar = null;
+            }
+
+            return "El video se ha eliminado";
+        }
+
 
         public Nodo Siguiente()
         {
