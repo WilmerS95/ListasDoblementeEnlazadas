56cbd3e5fbf72695e4fb98272bc1c0bd3ea2d571
WilmerS95 <wilmerantoniosinay@gmail.com>
2024-04-10 20:01:34 -0600

    Implementación función Agregar Video antes de Video X

--- ListasDoblementeEnlazadas/Services/ListasDoblementeEnlazadas.cs
+++ ListasDoblementeEnlazadas/Services/ListasDoblementeEnlazadas.cs
@@ -1,4 +1,5 @@
 ﻿using ListasDoblementeEnlazadas.DTO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ListasDoblementeEnlazadas.Services
 {
@@ -32,6 +33,39 @@ namespace ListasDoblementeEnlazadas.Services
             }
             nodoActual = nuevoNodo;
             return "Se ha agregado al inicio de la lista";
+        }
+
+        public string AddBeforeVideoX(string videoX, Nodo nuevoNodo)
+        {
+            if (isEmpty)
+            {
+                return "La lista está vacía, no hay videos para eliminar";
+            }
+
+            Nodo nodoActual = primerNodo;
+
+            while (nodoActual != null)
+            {
+                if (nodoActual.informacion.ToString() == videoX)
+                {
+                    if (nodoActual == primerNodo)
+                    {
+                        nuevoNodo.ligaSiguiente = primerNodo;
+                        primerNodo.ligaAnterior = nuevoNodo;
+                        primerNodo = nuevoNodo;
+                    }
+                    else
+                    {
+                        nuevoNodo.ligaSiguiente = nodoActual;
+                        nuevoNodo.ligaAnterior = nodoActual.ligaAnterior;
+                        nodoActual.ligaAnterior.ligaSiguiente = nuevoNodo;
+                        nodoActual.ligaAnterior = nuevoNodo;
+                    }
+                    return "Se ha agregado el video antes del video específico.";
+                }
+                nodoActual = nodoActual.ligaSiguiente;
+            }
+            return "El link del video específico no se encontró en la lista.";
         }
 
         public Nodo Siguiente()
